2bf11f3d59951d5c0c66211eceeb789650b51029
Vitaly Ushakov <uvitaly3@gmail.com>
2020-04-22 22:57:09 +0300

    positions is done

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -15,6 +15,7 @@ export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy 
   form: FormGroup
   positions: Position[] = []
   loading = false
+  positionId = null
   modal: MaterialInstance
 
   constructor(private positionsService: PositionsService) { }
@@ -22,7 +23,7 @@ export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy 
   ngOnInit(): void {
     this.form = new FormGroup({
       name: new FormControl(null, Validators.required),
-      cost: new FormControl(null, [Validators.required, Validators.min(1)])
+      cost: new FormControl(1, [Validators.required, Validators.min(1)])
     })
     this.loading = true
     this.positionsService.fetch(this.categoryId).subscribe(
@@ -42,16 +43,29 @@ export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy 
   }
 
   onSelectPosition(position: Position) {
+    this.positionId = position._id
+    this.form.patchValue({
+      name: position.name,
+      cost: position.cost
+    })
     this.modal.open()
+    MaterialService.updateTextInput()
   }
 
   onAddPosition() {
+    this.positionId = null
+    this.form.patchValue({
+      name: null,
+      cost: 1
+    })
     this.modal.open()
+    MaterialService.updateTextInput()
   }
 
   onCancel() {
     this.modal.close()
   }
+
 
   onSubmit() {
     this.form.disable()
@@ -60,22 +74,53 @@ export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy 
       cost: this.form.value.cost,
       category: this.categoryId,
     }
-    this.positionsService.create(position).subscribe(
-      pos => {
-        MaterialService.toast('Position is created')
-        this.positions.push(pos)
-      },
-      error => {
-        MaterialService.toast(error.error.message)
-      },
-      () => {
-        this.form.enable()
-      }
-    )
+    const completed = () => {
+      this.form.enable()
+      this.form.reset({name: '', cost: 1})
+      this.modal.close()
+    }
+    if (this.positionId) {
+      position._id = this.positionId
+      this.positionsService.update(position).subscribe(
+        pos => {
+          const idx = this.positions.findIndex(p => p._id === pos._id)
+          this.positions[idx] = pos
+          MaterialService.toast('Position is changed')
+          this.positions.push(pos)
+        },
+        error => {
+          MaterialService.toast(error.error.message)
+        },
+        completed
+      )
+    } else {
+      this.positionsService.create(position).subscribe(
+        pos => {
+          MaterialService.toast('Position is created')
+          this.positions.push(pos)
+        },
+        error => {
+          MaterialService.toast(error.error.message)
+        },
+        completed
+      )
+    }
+
 
   }
 
-  onDeletePosition(position: Position) {
-
+  onDeletePosition(event: Event, position: Position) {
+    event.stopPropagation()
+    const decision = window.confirm(`Remove ${position.name}?`)
+    if (decision) {
+      this.positionsService.delete(position).subscribe(
+        (response) => {
+          const idx = this.positions.findIndex(p => p._id === position._id)
+          this.positions.splice(idx, 1)
+          MaterialService.toast(response.message)
+        },
+        error => MaterialService.toast(error.error.message)
+      )
+    }
   }
 }
--- client/src/app/shared/services/positions.service.ts
+++ client/src/app/shared/services/positions.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Position} from "../interfaces";
+import {Message, Position} from "../interfaces";
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +14,11 @@ export class PositionsService {
   }
   create(position: Position): Observable<Position> {
     return this.http.post<Position>('/api/position', position)
+  }
+  update(position: Position): Observable<Position> {
+    return this.http.patch<Position>(`/api/position/${position._id}`, position)
+  }
+  delete(position: Position): Observable<Message> {
+    return this.http.delete<Message>(`/api/position/${position._id}`)
   }
 }
